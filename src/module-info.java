654dbba92d6b9718bf6f75f90f2de4da03283f5e
david32768 <81751257+david32768@users.noreply.github.com>
2022-09-12 12:18:23 +0100

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module com.github.david32768.jynx {
+	exports com.github.david32768.jynx;
+	exports jynx2asm.ops;
+	requires org.objectweb.asm;
+	requires org.objectweb.asm.tree;
+	requires org.objectweb.asm.tree.analysis;
+	requires org.objectweb.asm.util;
+	uses jynx2asm.ops.MacroLib;
+	provides com.github.david32768.jynx.MacroLib
+		with jynx2asm.ops.StructuredMacroLib;
+}
